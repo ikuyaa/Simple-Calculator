fcf3b092ca0dd20f15b7638257e00e70a6a989c9
ikuyaa <ikuyaa0@gmail.com>
2024-05-08 08:18:53 -0500

    Added Color Changer

--- app/src/main/java/com/zachary/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/zachary/simplecalculator/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.zachary.simplecalculator
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.zachary.simplecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,6 +35,22 @@ class MainActivity : AppCompatActivity() {
 
         binding.resetButton.setOnClickListener{
             resetCalculator()
+        }
+
+        binding.blueButton.setOnClickListener{
+            setCalTheme(themeColor.blue)
+        }
+
+        binding.redButton.setOnClickListener{
+            setCalTheme(themeColor.red)
+        }
+
+        binding.pinkButton.setOnClickListener{
+            setCalTheme(themeColor.pink)
+        }
+
+        binding.greenButton.setOnClickListener{
+            setCalTheme(themeColor.green)
         }
     }
 
@@ -83,6 +98,45 @@ class MainActivity : AppCompatActivity() {
         binding.number2.text.clear()
         binding.answerText.text = "Answer: "
     }
+
+    private fun setCalTheme(theme: themeColor){
+
+        if(theme == themeColor.blue){
+            setTheme(R.style.Base_Theme_SimpleCalculator_Blue)
+            binding.addButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.lightblue))
+            binding.subButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.lightblue))
+            binding.multButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.lightblue))
+            binding.divButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.lightblue))
+            binding.resetButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.lightblue))
+            setContentView(binding.root)
+
+        }
+        else if (theme == themeColor.red){
+            setTheme(R.style.Base_Theme_SimpleCalculator_Red)
+            binding.addButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.red))
+            binding.subButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.red))
+            binding.multButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.red))
+            binding.divButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.red))
+            binding.resetButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.red))
+            setContentView(binding.root)
+        }
+        else if (theme == themeColor.pink){
+            binding.addButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.pink))
+            binding.subButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.pink))
+            binding.multButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.pink))
+            binding.divButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.pink))
+            binding.resetButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.pink))
+
+        }
+        else if(theme == themeColor.green){
+            binding.addButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.green))
+            binding.subButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.green))
+            binding.multButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.green))
+            binding.divButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.green))
+            binding.resetButton.background.setTint(ContextCompat.getColor(applicationContext, R.color.green))
+        }
+    }
 }
 
 enum class operatorTypes { add, sub, mult, div}
+enum class themeColor { blue, red, pink, green}
